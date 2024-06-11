1be6a5104dd4622ff6d68f2fe25cad6ce1862521
Georgis93 <georgiana.alina93@yahoo.com>
2024-06-11 20:43:07 +0200

    done VG

--- src/components/groupedit.js
+++ src/components/groupedit.js
@@ -3,31 +3,35 @@ import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import musicService from '../services/music-group-service';
 
- function GroupsDetailsEdit(props) {
+function GroupsDetailsEdit(props) {
   const params = useParams();
   const [group, setGroup] = useState({});
   const [editedGroup, setEditedGroup] = useState({});
+  const [successMessage, setSuccessMessage] = useState('');
 
   useEffect(() => {
     (async () => {
       const service = new musicService(`https://appmusicwebapinet8.azurewebsites.net/api`);
       const groupData = await service.readMusicGroupAsync(params.id);
       setGroup(groupData);
-      setEditedGroup({
-        ...groupData
-      });
+      setEditedGroup({ ...groupData });
     })();
   }, [params.id]);
 
   const handleChange = (e) => {
     const { id, value } = e.target;
+    console.log(`Changing ${id} to ${value}`);
     setEditedGroup({ ...editedGroup, [id]: value });
   };
 
   const handleSave = async () => {
-    const service = new musicService(`https://appmusicwebapinet8.azurewebsites.net/api`);
-    await service.updateMusicGroupAsync(params.id, editedGroup);
-    // Optionally, you can navigate the user to the group view page after saving
+    try {
+      const service = new musicService(`https://appmusicwebapinet8.azurewebsites.net/api`);
+      await service.updateMusicGroupAsync(params.id, editedGroup);
+      setSuccessMessage('Changes saved successfully!');
+    } catch (error) {
+      console.error('Error saving changes:', error);
+    }
   };
 
   return (
@@ -35,10 +39,7 @@ import musicService from '../services/music-group-service';
       <div className="col-md-7 col-lg-8">
         <form className="needs-validation">
           <div className="row g-3">
-            <div className="col-sm-6">
-              <label htmlFor="musicGroupId" className="form-label">Group Id</label>
-              <input type="text" className="form-control" id="musicGroupId" value={editedGroup?.musicGroupId} readOnly />
-            </div>
+           
             <div className="col-sm-6">
               <label htmlFor="name" className="form-label">Group</label>
               <input type="text" className="form-control" id="name" value={editedGroup?.name} onChange={handleChange} />
@@ -48,16 +49,13 @@ import musicService from '../services/music-group-service';
               <input type="text" className="form-control" id="establishedYear" value={editedGroup?.establishedYear} onChange={handleChange} />
             </div>
             <div className="col-sm-6">
-              <label htmlFor="strGenre" className="form-label">Genre</label>
-              <input type="text" className="form-control" id="strGenre" value={editedGroup?.strGenre} onChange={handleChange} />
-            </div>
-            <div className="col-sm-6">
-              <label htmlFor="numberartists" className="form-label">Number Artists</label>
-              <input type="number" className="form-control" id="numberArtists" value={editedGroup?.numberArtists} onChange={handleChange} />
-            </div>
-            <div className="col-sm-6">
-              <label htmlFor="numberalbums" className="form-label">Number Published Albums</label>
-              <input type="number" className="form-control" id="numberAlbums" value={editedGroup?.numberAlbums} onChange={handleChange} />
+              <label htmlFor="genre" className="form-label">Group Genre</label>
+              <select className="form-control" id="genre" value={editedGroup?.genre || ''} onChange={handleChange}>
+                <option value="0">Rock</option>
+                <option value="1">Blues</option>
+                <option value="2">Jazz</option>
+                <option value="3">Metall</option>
+              </select>
             </div>
           </div>
         </form>
@@ -65,9 +63,13 @@ import musicService from '../services/music-group-service';
           <button className="custom-btn" onClick={handleSave}>Save Changes</button>
           <Link to={`/groupview/${group?.musicGroupId}`} className="custom-btn ms-2">Cancel</Link>
         </div>
+        {successMessage && <p className="text-success">{successMessage}</p>}
       </div>
     </div>
   );
 }
+// strGenre can't be edit because is not in update (swagger), but Genre (Group -Genre named by me) can change strGenre by group. '
+// for correct genre , we should know groupGenre numbers so, that's why i made a option to choose one of them.
+// 0- rock; 1-blues; 2- jazz; 3-metall.
 
 export default GroupsDetailsEdit;
--- src/pages/musicgroups.js
+++ src/pages/musicgroups.js
@@ -137,26 +137,21 @@ export function ListPager(props) {
     </nav>
   )
 }
-
 export function List(props) {
   return (
-    <div className="row row-cols-1 row-cols-lg-6 align-items-stretch g-4 py-5">
-      <div className="col-md-7 col-lg-10">
+    <div className="row row-cols-1 row-cols-lg-1 align-items-stretch g-4 py-5">
+      <div className="col-12">
         <div className="row mb-2 text-center">
-          <div className="col-md-3 themed-grid-head-col">Name</div>
-          <div className="col-md-2 themed-grid-head-col">Established Year</div>
-          <div className="col-md-2 themed-grid-head-col">Genre</div>
-          <div className="col-md-2 themed-grid-head-col">Number of Albums</div>
-          <div className="col-md-2 themed-grid-head-col">Number of Artists</div>
+          <div className="col-md-4 themed-grid-head-col">Name</div>
+          <div className="col-md-3 themed-grid-head-col">Established Year</div>
+          <div className="col-md-3 themed-grid-head-col">Genre</div>
         </div>
         {props.groups?.pageItems?.map((b) => (
           <Link to={`/groupview/${b.musicGroupId}`} style={{ textDecoration: 'none' }} key={b.musicGroupId}>
             <div className="row mb-2 text-center">
-              <div className="col-md-3 themed-grid-col">{b.name}</div>
-              <div className="col-md-2 themed-grid-col">{b.establishedYear}</div>
-              <div className="col-md-2 themed-grid-col">{b.strGenre}</div>
-              <div className="col-md-2 themed-grid-col">{b.albums?.length || 0}</div>
-              <div className="col-md-2 themed-grid-col">{b.artists?.length || 0}</div>
+              <div className="col-md-4 themed-grid-col">{b.name}</div>
+              <div className="col-md-3 themed-grid-col">{b.establishedYear}</div>
+              <div className="col-md-3 themed-grid-col">{b.strGenre}</div>
             </div>
           </Link>
         ))}
